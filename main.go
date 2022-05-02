837f1149ce8e88cc8c5689c1c012e5997c62b969
Hannes <56559005+donni-h@users.noreply.github.com>
2022-05-02 09:36:24 +0200

    created request handlers that serve different URLs

--- main.go
+++ main.go
@@ -7,14 +7,36 @@ import (
 
 // "Hello from Snippetbox as the response body."
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Snippetbox"))
+	/*
+		Check if the current request URL path *exactly* matches "/"
+		If it doesn't call http.NotFound() to send 404 response
+		return is necessary, otherwise the rest would be executed as well
+	*/
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from Hannes' snippetbox"))
+}
+
+// SnippetView handler function
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a snippet."))
+}
+
+//snippetCreate handler function
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet."))
 
 }
+
 func main() {
 	// User the NewServeMux() (Router) function to initialize a new servemux, then
-	// register the home function as the handler for the "URL" pattern
+	// register the handler functions as the handler for the "URL" pattern
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 	/*
 		Use the http.ListenAndServe() function to start a new web server.
 		We paas in two parameters: The TCP network address to listen on (in this case ":4000")
